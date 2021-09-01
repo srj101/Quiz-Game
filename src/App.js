d83b85d647f794f2529231a75ffe4b66148af44b
SR Joy <salim15-3117@diu.edu.bd>
2021-09-01 07:30:04 +0600

    Quiz Game with no CSS

--- src/App.js
+++ src/App.js
@@ -1,66 +1,54 @@
-import './App.css';
-import  VideoList  from './Components/VideoList/VideoList.component';
-import { Component } from 'react';
+import "./App.css";
+import VideoList from "./Components/VideoList/VideoList.component";
+import { Component } from "react";
 
-class MyApp extends Component{
-
-  constructor(){
+class MyApp extends Component {
+  constructor() {
     super();
     this.state = {
       videos: [],
-      searchField: '',
-      key: 9
-    }
+      searchField: "",
+      key: 9,
+    };
   }
 
-  shuffle =  (array) => {
-    var currentIndex = array.length,  randomIndex;
-  
-    
+  shuffle = (array) => {
+    var currentIndex = array.length,
+      randomIndex;
+
     while (0 !== currentIndex) {
-  
-      
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
-      
+
       [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
-  
+
     return array;
+  };
+
+  componentDidMount() {
+    fetch("https://opentdb.com/api.php?amount=50")
+      .then((response) => response.json())
+      .then((datas) => this.setState({ videos: datas.results }));
   }
 
+  render() {
+    const { videos, searchField, key } = this.state;
 
-  componentDidMount(){
-    fetch('https://opentdb.com/api.php?amount=20&category=23&difficulty=easy&type=multiple')
-    .then(response => response.json())
-    .then(datas => this.setState({videos:datas.results}))
-  }
-
-  render(){
-    
-
-    const {videos,searchField,key} = this.state; 
-
-
-
-    const filteredvideos = videos.filter(video => {
-      return video.category.toLowerCase().includes(searchField.toLowerCase())  
+    const filteredvideos = videos.filter((video) => {
+      if (video.category.toLowerCase().includes(searchField.toLowerCase())) {
+        return video;
+      }
     });
 
-    // const answers = videos.map(video => {
-    //   this.shuffle(video.incorrect_answers.concat(video.correct_answer));
-    // }) 
-    // this.setState({answerss:answers})
-    // {console.log(this.state.answers)}
-
-    return(
+    return (
       <div className="App">
-        <input type="search" className="ssearch" placeholder="Search" onChange={ e => this.setState({ searchField: e.target.value }) }/>
-        <VideoList videos={filteredvideos} key={key}  />
+        <VideoList videos={filteredvideos} key={key} />
       </div>
-    )
+    );
   }
 }
 
--- src/Components/Video/Video.component.jsx
+++ src/Components/Video/Video.component.jsx
@@ -1,92 +1,69 @@
-import React from 'react';
-import { VideoCSS } from './Video.css';
-import jsxToString from 'jsx-to-string';
+import React, { useState } from "react";
+import { VideoCSS } from "./Video.css";
+import jsxToString from "jsx-to-string";
 let somekey = 1;
 function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/,/g,'&#039;');
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/,/g, "&#039;");
 }
+const shuffle = (array) => {
+  var currentIndex = array.length,
+    randomIndex;
 
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
 
-class Video extends React.Component{
-    constructor(){
-        super();
-
-        
-            
-      
-            // console.log(this.shuffle(singlevid.incorrect_answers.concat(singlevid.correct_answer)).find(singlevid.correct_answer));
-         
+  return array;
+};
+const Video = ({ key, video, answers }) => {
+  const [answer, setAnswer] = useState();
+  const [msg, setMsg] = useState("");
+  const choosenAns = (e, cr) => {
+    setAnswer(e.value);
+    e.append(cr);
+    if (e.value != cr) {
+      setMsg("oooo");
+    } else {
+      setMsg("");
     }
+    console.log(msg.length > 1);
+  };
 
-    shuffle =  (array) => {
-        var currentIndex = array.length,  randomIndex;
-      
-        
-        while (0 !== currentIndex) {
-      
-          
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-      
-          
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
-      
-        return array;
-      }
-    
+  return (
+    <div className={msg.length > 1 ? "single-video ok" : "single-video"}>
+      <ul class="question-single">
+        <li dangerouslySetInnerHTML={{ __html: video.question }}></li>
+        <li>
+          <select
+            id="options"
+            onChange={(event) => choosenAns(event.target, video.correct_answer)}
+            value={answer}
+          >
+            <option value="">Select Correct Answer</option>
+            {answers.map((option, index) => {
+              return <option key={somekey++}>{option}</option>;
+            })}
+          </select>
+        </li>
+      </ul>
 
-    render(){
+      <hr />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+};
 
-    return(
-     
-        <div className="single-video-" >
-           
-           {
-            //    vec.push_back({"What's Your Father's Name?" , {"1. Foysal 2. Selim 3. Mf 4. Fufsdk",{1,"ans"}}});
-            }
-
-            
-
-            {`vec.push_back({"`}<span dangerouslySetInnerHTML={ {__html: this.props.video.question}}></span>{`" , {`}<span id="options">{`"`}
-        { this.props.answers.map((option,index)=>{
-                                return <span key={somekey++}>\n {index+1}{`. `}{option}{` `}{`\n`}<span id="spacing"></span></span>
-                            })}{`"`}{`,`}{ this.props.answers.map((option,index)=>{
-                                if(option == this.props.video.correct_answer) 
-                                {
-                                return <span key={somekey++}>{`{`}{index+1}{`,"`}{option}{`"`}{`}`}</span>
-                                }
-                            })}
-            </span>{`}});`}
-
-
-
-            {/* <hr />
-            <br />
-            <hr />
-                <ul>
-                <li>Type : { htmlEntities(props.video.type) } </li>
-                <li id="question" dangerouslySetInnerHTML={ {__html: props.video.question} }></li>
-                <li><em>Category : {props.video.category}</em></li>
-                <li id="answer" dangerouslySetInnerHTML={ {__html: props.video.correct_answer} }></li>
-
-
-                <li>
-                    options : 
-                        <ul>
-                            { props.video.incorrect_answers.map(option=>{
-                                return <li key={somekey+1}>{option}</li>
-                            })}
-                        </ul>
-                </li>
-            </ul> */}
-                
-        </div>
-    )
-    }
-        
-        }
- 
 export default Video;
